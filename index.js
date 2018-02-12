3f13068320e8cbc1864ae101cc3de36503ad9bf8
theuzer <jarvandemacio@gmail.com>
2018-02-12 16:38:35 +0000

    cors

--- index.js
+++ index.js
@@ -19,6 +19,12 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.listen(port, (err) => {
   if (err) {
     console.log(err);
