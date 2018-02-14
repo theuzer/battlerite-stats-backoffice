a1b375ef88a231c792f68f48bcf0cfa74982d61f
theuzer <jarvandemacio@gmail.com>
2018-02-14 17:10:09 +0000

    fix

--- controllers/characterController.js
+++ controllers/characterController.js
@@ -6,6 +6,10 @@ const dataConnection = require('../database/index').dataConnection;
 exports.getCharacterHistory = (req, res) => {
   // validate req ?
 
+  if (!req.query.page) {
+    req.query.page = '0';
+  }
+
   new sql.Request(dataConnection).query(queries.getCharacterHistory(req.query.playerId, req.query.page))
     .then((response) => {
       res.json(response.recordset);
