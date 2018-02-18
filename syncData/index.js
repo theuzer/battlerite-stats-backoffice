4c379517f9f6e9be2356f236dce8c0bb27410108
theuzer <jarvandemacio@gmail.com>
2018-02-18 16:45:32 +0000

    devs

--- syncData/index.js
+++ syncData/index.js
@@ -2,11 +2,6 @@ const constants = require('../common/constants');
 const syncStats = require('./syncStats');
 const syncPlayers = require('./syncPlayers');
 
-const delayLog = (logType, year, month, day, delayInSeconds) =>
-  setTimeout(() => {
-    syncStats.initializeLog(logType, year, month, day);
-  }, delayInSeconds * 1000);
-
 exports.initializeDb = () => {
   const currDate = new Date();
   const year = currDate.getFullYear();
@@ -14,9 +9,9 @@ exports.initializeDb = () => {
   const day = currDate.getDate();
 
   syncStats.initializeLog(constants.logType.yesterday, year, month, day);
-  delayLog(constants.logType.lastWeek, year, month, day, 60);
-  delayLog(constants.logType.lastMonth, year, month, day, 120);
-  delayLog(constants.logType.allTime, year, month, day, 180);
+  setTimeout(syncStats.initializeLog(constants.logType.lastWeek, year, month, day), 60000);
+  setTimeout(syncStats.initializeLog(constants.logType.lastMonth, year, month, day), 120000);
+  setTimeout(syncStats.initializeLog(constants.logType.allTime, year, month, day), 180000);
 
   syncPlayers.initializeLog();
 };
