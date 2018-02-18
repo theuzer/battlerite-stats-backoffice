c273a69130a458c9aaedbb1ee775f906bc901a29
theuzer <jarvandemacio@gmail.com>
2018-02-18 16:12:18 +0000

    fix

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const syncData = require('./syncData/index');
 const dataConnection = require('./database/index').dataConnection;
 require('./database/index');
 
-const port = 3000; // || process.env.PORT
+const port = process.env.PORT || 3000;
 
 const app = express();
 
